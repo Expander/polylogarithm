6c74a1b36dc8b07426c6a8456a028da357c403f9
Alexander Voigt <alexander.voigt@desy.de>
2015-09-11 16:08:03 +0200

    initial commit

---
+++ dilog.hpp
@@ -0,0 +1,23 @@
+// ====================================================================
+// This file is part of Dilogarithm.
+//
+// Dilogarithm is licenced under the GNU Lesser General Public License
+// (GNU LGPL) version 3.
+// ====================================================================
+
+#ifndef DILOG_H
+#define DILOG_H
+
+#include <complex>
+
+namespace dilogarithm {
+
+/// real dilogarithm
+double dilog(double);
+
+/// complex dilogarithm
+std::complex<double> dilog(const std::complex<double>&);
+
+} // namespace dilogarithm
+
+#endif
