4cff522d3d43c28bcf5525f73a0a8d4b1ccbf681
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-02-20 12:46:13 +0100

    adding Mathematica implementation of the polylogarithm
    series expansion around z = 1.

---
+++ test/dilog.m
@@ -0,0 +1,4 @@
+u[x_] := -Log[1 - x];
+X[0, n_] := BernoulliB[n];
+X[p_, n_] := Sum[Binomial[n, k] BernoulliB[n - k]/(k + 1) X[p - 1, k], {k, 0, n}];
+Li[p_, x_, n_:100] := Sum[X[p - 2, n] u[x]^(n + 1)/Factorial[n + 1], {n, 0, n}];
