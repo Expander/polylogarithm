396e1420d466c1943f70ac84fcd13108bf283cde
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-02-23 09:48:00 +0100

    implement polylogarithm with n=5

--- dilog.hpp
+++ dilog.hpp
@@ -36,6 +36,9 @@ std::complex<double> Li3(const std::complex<double>&);
 /// complex polylogarithm with n=4
 std::complex<double> Li4(const std::complex<double>&);
 
+/// complex polylogarithm with n=5
+std::complex<double> Li5(const std::complex<double>&);
+
 // Clausen function
 double Cl2(double);
 
