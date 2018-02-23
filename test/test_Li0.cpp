70b8c8926ca54f13fd2ad64d120651b827fbfb3a
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-02-23 14:15:13 +0100

    adding test for Li0

---
+++ test/test_Li0.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 1
+
+#include "doctest.h"
+#include "dilog.hpp"
+#include <cmath>
+#include <utility>
+
+#define CHECK_CLOSE(a,b,eps) CHECK((a) == doctest::Approx(b).epsilon(eps))
+#define CHECK_CLOSE_COMPLEX(a,b,eps) do {                               \
+      CHECK(std::real(a) == doctest::Approx(std::real(b)).epsilon(eps)); \
+      CHECK(std::imag(a) == doctest::Approx(std::imag(b)).epsilon(eps)); \
+   } while (0);
+#define CHECK_SMALL(a,eps) CHECK(std::abs(a) < (eps))
+
+TEST_CASE("test_special_values")
+{
+   using dilogarithm::Li0;
+
+   CHECK_SMALL(Li0(0.), 1e-15);
+   CHECK(!std::isfinite(Li0(1)));
+}
