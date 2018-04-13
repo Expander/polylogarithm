d51018fa699bf1d230da607aa61b06abfdc00646
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-04-13 16:28:09 +0200

    return inf and not nan if z = 1 for Li0(z)

--- src/Li0.cpp
+++ src/Li0.cpp
@@ -6,8 +6,27 @@
 // ====================================================================
 
 #include "Li0.hpp"
+#include <cmath>
+#include <limits>
 
 namespace polylogarithm {
+
+namespace {
+   const double epsilon = std::pow(10., -std::floor(std::numeric_limits<double>::digits10));
+   const double inf = std::numeric_limits<double>::infinity();
+
+   bool is_close(double a, double b, double eps = epsilon)
+   {
+      return std::abs(a - b) < eps;
+   }
+
+   bool is_close(const std::complex<double>& a, const std::complex<double>& b,
+                 double eps = epsilon)
+   {
+      return std::abs(std::real(a) - std::real(b)) < eps &&
+             std::abs(std::imag(a) - std::imag(b)) < eps;
+   }
+} // anonymous namespace
 
 /**
  * @brief Real polylogarithm \f$\mathrm{Li}_0(x) = x/(1-x)\f$
@@ -16,6 +35,9 @@ namespace polylogarithm {
  */
 double Li0(double x)
 {
+   if (is_close(x, 1.))
+      return inf;
+
    return x/(1. - x);
 }
 
@@ -26,6 +48,9 @@ double Li0(double x)
  */
 std::complex<double> Li0(const std::complex<double>& z)
 {
+   if (is_close(z, {1.,0.}))
+      return {inf, inf};
+
    return z/(1. - z);
 }
 
