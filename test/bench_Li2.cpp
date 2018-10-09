d275bb4bf89cf2605ec1c82bd510b10a7f3509cc
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-10-09 08:08:21 +0200

    bench chunk of points, as clock has not enough resolution

--- test/bench_Li2.cpp
+++ test/bench_Li2.cpp
@@ -8,11 +8,12 @@ void bench_real_Li2(std::size_t N, double min, double max)
    using namespace polylogarithm::bench;
 
    const auto values = generate_random_doubles(N, min, max);
-   double total_time = 0.;
 
-   for (const auto& v: values) {
-      total_time += time([v] { return polylogarithm::Li2(v); });
-   }
+   const double total_time = time([&values] {
+         for (const auto& v: values) {
+            volatile auto li = polylogarithm::Li2(v);
+         }
+      });
 
    std::cout << "Evaluation of real Li2 " << N << " times took: "
              << total_time*1000. << "ms\n";
@@ -24,13 +25,14 @@ void bench_cmpl_Li2(std::size_t N, double min, double max)
    using namespace polylogarithm::bench;
 
    const auto values = generate_random_complexes(N, min, max);
-   double total_time = 0.;
 
-   for (const auto& v: values) {
-      total_time += time([v] { return polylogarithm::Li2(v); });
-   }
+   const double total_time = time([&values] {
+         for (const auto& v: values) {
+            volatile auto li = polylogarithm::Li2(v);
+         }
+      });
 
-   std::cout << "Evaluation of complex Li2 " << N << " times took: "
+   std::cout << "Evaluation of cmpl Li2 " << N << " times took: "
              << total_time*1000. << "ms\n";
 }
 
--- test/bench_Li3.cpp
+++ test/bench_Li3.cpp
@@ -2,16 +2,18 @@
 #include "Li3.hpp"
 #include <iostream>
 
+
 void bench_Li3(std::size_t N, double min, double max)
 {
    using namespace polylogarithm::bench;
 
    const auto values = generate_random_complexes(N, min, max);
-   double total_time = 0.;
 
-   for (const auto& v: values) {
-      total_time += time([v] { return polylogarithm::Li3(v); });
-   }
+   const double total_time = time([&values] {
+         for (const auto& v: values) {
+            volatile auto li = polylogarithm::Li3(v);
+         }
+      });
 
    std::cout << "Evaluation of complex Li3 " << N << " times took: "
              << total_time*1000. << "ms\n";
