e8b22b0f76f6278ecf85e02efe55e3eb6123d52a
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-10-09 11:07:11 +0200

    version 3.2.1

--- src/version.hpp
+++ src/version.hpp
@@ -12,7 +12,7 @@ namespace polylogarithm {
 namespace {
    const int version_major = 3;
    const int version_minor = 2;
-   const int version_patch = 0;
+   const int version_patch = 1;
 }
 
 } // namespace polylogarithm
