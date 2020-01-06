e2693798812b6e1f0f4a272191bc88dcbc078ac3
Alexander Voigt <ahgvoigt@gmail.com>
2020-01-06 16:45:22 +0100

    implementation of real and complex dilogarithm with long double precision

--- src/Li2.hpp
+++ src/Li2.hpp
@@ -16,7 +16,13 @@ double Cl2(double);
 /// real polylogarithm with n=2 (dilogarithm)
 double Li2(double);
 
+/// real dilogarithm with n=2 (dilogarithm)
+long double Li2(long double);
+
 /// complex polylogarithm with n=2 (dilogarithm)
 std::complex<double> Li2(const std::complex<double>&);
+
+/// complex polylogarithm with n=2 (dilogarithm)
+std::complex<long double> Li2(const std::complex<long double>&);
 
 } // namespace polylogarithm
