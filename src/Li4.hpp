0e843eb2b34c69ba00fa1972ef57a16672c0337d
Alexander Voigt <ahgvoigt@gmail.com>
2020-01-15 10:54:01 +0100

    extending documentation

--- src/Li4.hpp
+++ src/Li4.hpp
@@ -16,7 +16,7 @@ double Cl4(double);
 /// complex polylogarithm with n=4
 std::complex<double> Li4(const std::complex<double>&);
 
-/// complex polylogarithm with n=4
+/// complex polylogarithm with n=4 with long double precision
 std::complex<long double> Li4(const std::complex<long double>&);
 
 } // namespace polylogarithm
