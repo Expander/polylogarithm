d0f8b66289feb47bbf08f033168ace7ee1291c6f
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-06 15:40:14 +0200

    adding C implementations of real and complex dilogarithm
    with double precision

---
+++ src/Li2.h
@@ -0,0 +1,22 @@
+// ====================================================================
+// This file is part of Polylogarithm.
+//
+// Polylogarithm is licenced under the GNU Lesser General Public
+// License (GNU LGPL) version 3.
+// ====================================================================
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** real polylogarithm with n=2 (dilogarithm) */
+double li2(double x);
+
+/** complex polylogarithm with n=2 (dilogarithm) */
+void cli2_(double re, double im, double* res_re, double* res_im);
+
+#ifdef __cplusplus
+}
+#endif
