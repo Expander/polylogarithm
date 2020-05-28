8ac946d03ccce483bc793c6f972004799893f9ca
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-28 21:00:41 +0200

    use own and fast implementation of complex numbers

---
+++ src/complex.hpp
@@ -0,0 +1,105 @@
+// ====================================================================
+// This file is part of Polylogarithm.
+//
+// Polylogarithm is licenced under the GNU Lesser General Public
+// License (GNU LGPL) version 3.
+// ====================================================================
+
+#pragma once
+
+#include <cmath>
+
+namespace polylogarithm {
+
+template <typename T>
+struct Complex {
+   constexpr Complex(T re_ = T{}, T im_ = T{}) : re(re_), im(im_) {}
+   T re{};
+   T im{};
+};
+
+template <typename T>
+constexpr Complex<T> conj(const Complex<T>& z) noexcept
+{
+   return { z.re, -z.im };
+}
+
+template <typename T>
+constexpr Complex<T> log(const Complex<T>& z) noexcept
+{
+   return { 0.5*std::log(z.re*z.re + z.im*z.im), std::atan2(z.im, z.re) };
+}
+
+template <typename T>
+constexpr T norm_sqr(const Complex<T>& z) noexcept
+{
+   return z.re*z.re + z.im*z.im;
+}
+
+template <typename T>
+constexpr Complex<T> operator+(const Complex<T>& a, const Complex<T>& b) noexcept
+{
+   return { a.re + b.re, a.im + b.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator+(const Complex<T>& z, T x) noexcept
+{
+   return { z.re + x, z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator+(T x, const Complex<T>& z) noexcept
+{
+   return { x + z.re, z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator-(const Complex<T>& a, const Complex<T>& b) noexcept
+{
+   return { a.re - b.re, a.im - b.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator-(T x, const Complex<T>& z) noexcept
+{
+   return { x - z.re, -z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator-(const Complex<T>& z, T x) noexcept
+{
+   return { z.re - x, z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator-(const Complex<T>& z) noexcept
+{
+   return { -z.re, -z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator*(const Complex<T>& a, const Complex<T>& b) noexcept
+{
+   return { a.re*b.re - a.im*b.im, a.re*b.im + a.im*b.re };
+}
+
+template <typename T>
+constexpr Complex<T> operator*(T x, const Complex<T>& z) noexcept
+{
+   return { x*z.re, x*z.im };
+}
+
+template <typename T>
+constexpr Complex<T> operator/(T x, const Complex<T>& z) noexcept
+{
+   return x*conj(z)/norm_sqr(z);
+}
+
+template <typename T>
+constexpr Complex<T> operator/(const Complex<T>& z, T x) noexcept
+{
+   return { z.re/x, z.im/x };
+}
+
+} // namespace polylogarithm
