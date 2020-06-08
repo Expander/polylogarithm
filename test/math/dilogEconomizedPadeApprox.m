d7c272a68c7611b448c7aa75d5547942c8487bfb
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-08 11:21:28 +0200

    bring rational function to standard form

--- test/math/dilogEconomizedPadeApprox.m
+++ test/math/dilogEconomizedPadeApprox.m
@@ -14,18 +14,28 @@ half = (interval[[2]] - interval[[1]])/2;
 (* series for PolyLog[2,x]/x *)
 Li2x[x_, n_:nMax] := Sum[x^(i-1)/i^2, {i,1,n}];
 
-appox = EconomizedRationalApproximation[Li2x[x], {x, interval, 7, 7}] /. (-half + x) -> y;
+(* bring rational function to standard form *)
+PolynomialStandardForm[expr_, x_] :=
+    Module[{n = Numerator[expr], d = Denominator[expr], c},
+           c = d /. x -> 0;
+           Expand[n/c] / Expand[d/c]
+    ];
+
+approx = EconomizedRationalApproximation[Li2x[x], {x, interval, 7, 7}] /. (-half + x) -> y;
+
+approx = PolynomialStandardForm[approx, y];
 
 FormatCoeffs[expr_, x_, prec_] :=
     N[#, prec]& /@ CoefficientList[expr, x]
 
 Print["Numerator coefficients: ",
-      FormatCoeffs[#,y,outPrec]& @ Numerator[appox]];
+      FormatCoeffs[#,y,outPrec]& @ Numerator[approx]];
 
 Print["Denominator coefficients: ",
-      FormatCoeffs[#,y,outPrec]& @ Denominator[appox]];
+      FormatCoeffs[#,y,outPrec]& @ Denominator[approx]];
 
-diff = (appox - Li2x[x]) /. y -> x - half;
+(* calculate maximum error *)
+diff = (approx - Li2x[x]) /. y -> x - half;
 
 maxErr = Max @ Abs @ Table[N[diff, outPrec] /. x -> 1/2*n/10, {n, 0, 10}];
 
