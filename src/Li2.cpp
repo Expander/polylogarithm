e166e955a3af0a5931b4d77149e7b9dd92df6c79
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-09 17:26:43 +0200

    simplify if-else-statement

--- src/Li2.cpp
+++ src/Li2.cpp
@@ -375,9 +375,8 @@ std::complex<long double> Li2(const std::complex<long double>& z_) noexcept
       if (z.re <= 1) {
          return Li2(z.re);
       }
-      if (z.re > 1) {
-         return { Li2(z.re), -PI*std::log(z.re) };
-      }
+      // z.re > 1
+      return { Li2(z.re), -PI*std::log(z.re) };
    } else if (nz < std::numeric_limits<long double>::epsilon()) {
       return z_;
    }
