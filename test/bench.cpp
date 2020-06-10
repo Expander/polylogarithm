8036724744d945e17f4cff4de97875a1d1a3a588
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-10 12:20:48 +0200

    rename test

--- test/bench.cpp
+++ test/bench.cpp
@@ -2,13 +2,13 @@
 #include <benchmark/benchmark.h>
 
 template <class ...ExtraArgs>
-static void BM_Li2_real_poly_cpp(benchmark::State& state, ExtraArgs&&... extra_args)
+static void BM_Li2_poly_cpp(benchmark::State& state, ExtraArgs&&... extra_args)
 {
   for (auto _ : state) {
      benchmark::DoNotOptimize(polylogarithm::Li2(extra_args...));
   }
 }
 
-BENCHMARK_CAPTURE(BM_Li2_real_poly_cpp, double_test_unity, 1.1);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, 1.1);
 
 BENCHMARK_MAIN();
