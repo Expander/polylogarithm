d2f073989452f215687306e7e4a0ba78764a92d8
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-10 12:12:31 +0200

    adding benchmark test stub for real Li2 using Google benchmark

---
+++ test/bench.cpp
@@ -0,0 +1,13 @@
+#include "Li2.hpp"
+#include <benchmark/benchmark.h>
+
+static void BM_Li2_real_poly_cpp(benchmark::State& state)
+{
+  for (auto _ : state) {
+     polylogarithm::Li2(1.1);
+  }
+}
+
+BENCHMARK(BM_Li2_real_poly_cpp);
+
+BENCHMARK_MAIN();
