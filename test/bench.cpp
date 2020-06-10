4916b50c33d73b70fbb234bce01cf640bfe0230c
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-10 12:24:28 +0200

    test all transformations

--- test/bench.cpp
+++ test/bench.cpp
@@ -9,6 +9,33 @@ static void BM_Li2_poly_cpp(benchmark::State& state, ExtraArgs&&... extra_args)
   }
 }
 
-BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, 1.1);
+
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.1);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.4);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.5);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.6);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  0.9);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  1.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  1.2);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  1.5);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  1.9);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  2.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  2.1);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity,  3.0);
+
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.1);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.4);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.5);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.6);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -0.9);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -1.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -1.2);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -1.5);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -1.9);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -2.0);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -2.1);
+BENCHMARK_CAPTURE(BM_Li2_poly_cpp, double_unity, -3.0);
 
 BENCHMARK_MAIN();
