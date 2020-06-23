4111217012dfc93c2297b395e7d59546ec8c275c
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-23 15:13:03 +0200

    rename Fortran wrapper

--- test/alt/fortran/cwrappers.f90
+++ test/alt/fortran/cwrappers.f90
@@ -1,8 +1,8 @@
-subroutine li2_fortran(x, res) bind(C, name="li2_fortran_")
+subroutine dli2_wrapper(x, res) bind(C, name="dli2_wrapper")
   implicit none
   double precision, intent(in)  :: x
   double precision, intent(out) :: res
   double precision dli2
 
   res = dli2(x)
-end subroutine li2_fortran
+end subroutine dli2_wrapper
