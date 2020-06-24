e48e632fae852d1bb0537b402385224229adefdc
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-24 10:46:10 +0200

    use proper ISO C binding

--- test/alt/fortran/cwrappers.f90
+++ test/alt/fortran/cwrappers.f90
@@ -1,16 +1,18 @@
-subroutine dli2_wrapper(x, res) bind(C, name="dli2_wrapper")
+subroutine dli2_wrapper(x, res) bind(C)
+  use, intrinsic :: iso_c_binding
   implicit none
-  double precision, intent(in)  :: x
-  double precision, intent(out) :: res
+  real(c_double), intent(in)  :: x
+  real(c_double), intent(out) :: res
   double precision dli2
   res = dli2(x)
 end subroutine dli2_wrapper
 
 
-subroutine cdli2_wrapper(re, im, res_re, res_im) bind(C, name="cdli2_wrapper")
+subroutine cdli2_wrapper(re, im, res_re, res_im) bind(C)
+  use, intrinsic :: iso_c_binding
   implicit none
-  double precision, intent(in)  :: im, re
-  double precision, intent(out) :: res_re, res_im
+  real(c_double), intent(in)  :: im, re
+  real(c_double), intent(out) :: res_re, res_im
   double complex res, cdli2
   res = cdli2(dcmplx(re, im))
   res_re = real(res)
