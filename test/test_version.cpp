91fc5e0aa047261e7eb82395217d1529de48664c
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-27 19:48:06 +0200

    use C/C++/Fortran compatible version.h file

--- test/test_version.cpp
+++ test/test_version.cpp
@@ -1,16 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 1
 
 #include "doctest.h"
-#include "version.hpp"
+#include "version.h"
 #include <iostream>
 
 TEST_CASE("test_version_string")
 {
-   using polylogarithm::version_major;
-   using polylogarithm::version_minor;
-   using polylogarithm::version_patch;
-
-   std::cout << "Polylogarithm version: " << version_major << "."
-             << version_minor << "." << version_patch << std::endl;
+   std::cout << "Polylogarithm version: " << POLYLOGARITHM_MAJOR << "."
+             << POLYLOGARITHM_MINOR << "." << POLYLOGARITHM_PATCH << std::endl;
    CHECK(true);
 }
