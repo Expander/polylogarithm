db5ec563ef6fd30a27af24db5c127a77be376e89
Alexander Voigt <ahgvoigt@gmail.com>
2021-07-17 10:14:58 +0200

    adding benchmark for Clausen functions

---
+++ test/bench_Cl.cpp
@@ -0,0 +1,73 @@
+#include "alt.h"
+#include "bench.hpp"
+#include "c_wrappers.h"
+#include "Cl2.hpp"
+#include <iostream>
+#include <iomanip>
+
+namespace {
+
+#ifdef ENABLE_GSL
+
+#include <gsl/gsl_sf_clausen.h>
+
+double gsl_cl2(double x) {
+   return gsl_sf_clausen(x);
+}
+
+#endif
+
+} // anonymous namespace
+
+template <typename T, typename Fn>
+void bench_fn(Fn f, const std::vector<T>& values, const std::string& name,
+              const std::string& type)
+{
+   const auto total_time = polylogarithm::bench::time_in_seconds([&] {
+         for (const auto& v: values) {
+            polylogarithm::bench::do_not_optimize(f(v));
+         }
+      });
+
+   std::cout << std::setw(24) << std::left << name << "type: " << std::setw(16)
+             << std::left << type << "time: " << total_time << "s\n";
+}
+
+void print_line()
+{
+   std::cout << "----------------------------------------------------------------\n";
+}
+
+void print_headline(const std::string& text)
+{
+   print_line();
+   std::cout << text << '\n';
+   print_line();
+}
+
+int main() {
+   using polylogarithm::bench::generate_random_scalars;
+   using polylogarithm::bench::generate_random_complexes;
+
+   const std::size_t N = 1000000;
+   const auto min = 0.0;
+   const auto max = 3.1415926535897932;
+
+   const auto values_d  = generate_random_scalars<double>(N, min, max);
+   const auto values_l  = generate_random_scalars<long double>(N, min, max);
+
+   print_headline("Cl2");
+
+   bench_fn([&](double x) { return polylogarithm::Cl2(x); }, values_d,
+            "polylogarithm C++", "double");
+
+#ifdef ENABLE_GSL
+   bench_fn([&](double x) { return gsl_cl2(x); }, values_d,
+            "GSL", "double");
+#endif
+
+   bench_fn([&](long double x) { return polylogarithm::Cl2(x); }, values_l,
+            "polylogarithm C++", "long double");
+
+   return 0;
+}
