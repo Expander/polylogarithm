1f2fae7008192eac4ce036e2abe4567d44f31926
Alexander Voigt <ahgvoigt@gmail.com>
2021-08-20 18:41:58 +0200

    adding warm-up and test performance of range deduction

--- test/bench_Cl.cpp
+++ test/bench_Cl.cpp
@@ -31,6 +31,11 @@ template <typename T, typename Fn>
 void bench_fn(Fn f, const std::vector<T>& values, const std::string& name,
               const std::string& type)
 {
+   // warm-up
+   for (const auto& v: values) {
+      polylogarithm::bench::do_not_optimize(f(v));
+   }
+
    const auto total_time = polylogarithm::bench::time_in_seconds([&] {
          for (const auto& v: values) {
             polylogarithm::bench::do_not_optimize(f(v));
@@ -58,8 +63,9 @@ int main() {
    using polylogarithm::bench::generate_random_complexes;
 
    const std::size_t N = 1000000;
-   const auto min = 0.0;
-   const auto max = 3.1415926535897932;
+   const auto pi = 3.1415926535897932;
+   const auto min = -8*pi;
+   const auto max = 8*pi;
 
    const auto values_d  = generate_random_scalars<double>(N, min, max);
    const auto values_l  = generate_random_scalars<long double>(N, min, max);
