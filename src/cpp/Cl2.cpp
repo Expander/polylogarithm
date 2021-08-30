fc8fc681e1811a4dcaa814e6546e7cb1813bb7a7
Alexander Voigt <ahgvoigt@gmail.com>
2021-08-30 12:36:50 +0200

    use Pade approximation as default C++ implementation for Cl2

--- src/cpp/Cl2.cpp
+++ src/cpp/Cl2.cpp
@@ -13,43 +13,13 @@ namespace polylogarithm {
  * @brief Clausen function \f$\mathrm{Cl}_2(\theta) = \mathrm{Im}(\mathrm{Li}_2(e^{i\theta}))\f$
  * @param x real angle
  * @return \f$\mathrm{Cl}_2(\theta)\f$
- * @author K.S. Kölbig
- * @note Implementation translated by Alexander Voigt from CERNLIB DCLAUS function C326
- *
- * Journal of Computational and Applied Mathematics 64 (1995) 295-297.
+ * @author Alexander Voigt
+ * @note Implementation as economized Padé approximation.
  */
 double Cl2(double x) noexcept
 {
    const double PI = 3.14159265358979324;
-   const double PI2 = 2*PI, PIH = PI/2, RPIH = 2/PI;
-   const double A[9] = {
-      0.0279528319735756613,
-      0.0001763088743898116,
-      0.0000012662741461157,
-      0.0000000117171818134,
-      0.0000000001230064129,
-      0.0000000000013952729,
-      0.0000000000000166908,
-      0.0000000000000002076,
-      0.0000000000000000027
-   };
-   const double B[14] = {
-       0.639097088857265341,
-      -0.054980569301851716,
-      -0.000961261945950606,
-      -0.000032054686822550,
-      -0.000001329461695426,
-      -0.000000062093601824,
-      -0.000000003129600656,
-      -0.000000000166351954,
-      -0.000000000009196527,
-      -0.000000000000524004,
-      -0.000000000000030580,
-      -0.000000000000001820,
-      -0.000000000000000110,
-      -0.000000000000000007,
-   };
-
+   const double PI2 = 2*PI, PIH = PI/2, PI28 = PI*PI/8;
    double sgn = 1;
 
    if (x < 0) {
@@ -75,27 +45,42 @@ double Cl2(double x) noexcept
    double h = 0;
 
    if (x < PIH) {
-      const double u = RPIH*x;
-      h = 2*u*u - 1;
-      const double alfa = h + h;
-      double b0 = 0, b1 = 0, b2 = 0;
-      for (int i = 8; i >= 0; i--) {
-         b0 = A[i] + alfa*b1 - b2;
-         b2 = b1;
-         b1 = b0;
-      }
-      h = x*(1 - std::log(x) + x*x*(b0 - h*b2)/2);
+      const double P[] = {
+         2.7951565822419270e-02, -8.8865360514541522e-04,
+         6.8282348222485902e-06, -7.5276232403566808e-09
+      };
+      const double Q[] = {
+         1.0000000000000000e+00, -3.6904397961160525e-02,
+         3.7342870576106476e-04, -8.7460760866531179e-07
+      };
+      const double y = x*x;
+      const double z = y - PI28;
+      const double z2 = z*z;
+      const double p = P[0] + z * P[1] + z2 * (P[2] + z * P[3]);
+      const double q = Q[0] + z * Q[1] + z2 * (Q[2] + z * Q[3]);
+
+      h = x*(1 - std::log(x) + y*p/q/2);
    } else {
-      const double u = RPIH*x - 2;
-      h = 2*u*u - 1;
-      const double alfa = h + h;
-      double b0 = 0, b1 = 0, b2 = 0;
-      for (int i = 13; i >= 0; i--) {
-         b0 = B[i] + alfa*b1 - b2;
-         b2 = b1;
-         b1 = b0;
-      }
-      h = (PI - x)*(b0 - h*b2);
+      const double P[] = {
+         6.4005702446195512e-01, -2.0641655351338783e-01,
+         2.4175305223497718e-02, -1.2355955287855728e-03,
+         2.5649833551291124e-05, -1.4783829128773320e-07
+      };
+      const double Q[] = {
+         1.0000000000000000e+00, -2.5299102015666356e-01,
+         2.2148751048467057e-02, -7.8183920462457496e-04,
+         9.5432542196310670e-06, -1.8184302880448247e-08
+      };
+      const double y = PI - x;
+      const double z = y*y - PI28;
+      const double z2 = z*z;
+      const double z4 = z2*z2;
+      const double p = P[0] + z * P[1] + z2 * (P[2] + z * P[3]) +
+         z4 * (P[4] + z * P[5]);
+      const double q = Q[0] + z * Q[1] + z2 * (Q[2] + z * Q[3]) +
+         z4 * (Q[4] + z * Q[5]);
+
+      h = y*p/q;
    }
 
    return sgn*h;
