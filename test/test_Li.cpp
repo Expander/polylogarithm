98824c6828c78248606846c43128a317f48043dc
Alexander Voigt <ahgvoigt@gmail.com>
2022-02-16 10:15:45 +0100

    adding (commented) tests

--- test/test_Li.cpp
+++ test/test_Li.cpp
@@ -14,10 +14,27 @@
       CHECK_CLOSE(std::imag(a), std::imag(b), (eps));                   \
    } while (0)
 
+/*
+
+TEST_CASE("test_infinite_values")
+{
+   using polylogarithm::Li;
+
+   for (int n = -10; n <= 0; ++n) {
+      CHECK(std::isinf(std::real(Li(n, 1.0))));
+      CHECK(std::isinf(std::imag(Li(n, 1.0))));
+   }
+}
+
+*/
+
 TEST_CASE("test_complex_fixed_values")
 {
    const auto eps = 1e-12;
-   const int ni[] = {-2, -1, 0, 1, 2, 3, 4, 5, 6, 100};
+   const int ni[] = {
+      // -100, -10, -9, -8, -7, -6, -5, -4, -3,
+      -2, -1, 0, 1, 2, 3, 4, 5, 6, 100
+   };
 
    for (const auto n: ni) {
       const std::string filename(std::string(TEST_DATA_DIR) + PATH_SEPARATOR + "Li" + std::to_string(n) + ".txt");
@@ -26,8 +43,9 @@ TEST_CASE("test_complex_fixed_values")
       for (auto v: values) {
          const auto z = v.first;
          const auto li_expected = v.second;
+         const auto li = polylogarithm::Li(n,z);
          INFO("n = " << n << ", z = " << z);
-         CHECK_CLOSE_COMPLEX(polylogarithm::Li(n,z), li_expected, eps);
+         CHECK_CLOSE_COMPLEX(li, li_expected, eps);
       }
    }
 }
