77b592b63e622a621b44e446599e76b8f3f14954
Alexander Voigt <ahgvoigt@gmail.com>
2022-03-01 17:12:03 +0100

    adding test that is sensitive to the treatment of -0.0 and 0.0
    in the imaginary part of z

--- test/test_Li.cpp
+++ test/test_Li.cpp
@@ -30,6 +30,8 @@ TEST_CASE("test_infinite_values")
 
 TEST_CASE("test_complex_fixed_values")
 {
+   using polylogarithm::Li;
+
    const struct {
       int n;
       double eps;
@@ -62,12 +64,16 @@ TEST_CASE("test_complex_fixed_values")
       for (auto v: values) {
          const auto z = v.first;
          const auto li_expected = v.second;
-         const auto li = polylogarithm::Li(ni.n, z);
+         const auto li = Li(ni.n, z);
          INFO("n = " << ni.n << ", z = " << z);
          CHECK_CLOSE_COMPLEX(li, li_expected, ni.eps);
       }
    }
 
    // value close to boundary between series 1 and 2 in arXiv:2010.09860
-   CHECK_CLOSE_COMPLEX(polylogarithm::Li(-2, -0.50001), -0.074072592582716422, 1e-14);
+   CHECK_CLOSE_COMPLEX(Li(-2, -0.50001), -0.074072592582716422, 1e-14);
+
+   const auto z = std::complex<double>(1.5, 0.0);
+   CHECK_CLOSE_COMPLEX(Li(10, z), std::complex<double>(1.5022603281703005298, -2.56429642116111388671e-9), 1e-14);
+   CHECK_CLOSE_COMPLEX(Li(10, -z), std::complex<double>(-1.4978556954869267594, 0.0), 1e-14);
 }
