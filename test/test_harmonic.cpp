d708a1029391c9526fd422c4573792407bdd20a0
Alexander Voigt <ahgvoigt@gmail.com>
2022-11-14 08:25:26 +0100

    allow for small epsilon deviation in harmonic numbers
    Note: Due to potential compiler optimizations / re-arrangements of
    instructions such small deviations can occur. Such deviations
    have been observed on macOS-latest -Ofast -ffast-math

--- test/test_harmonic.cpp
+++ test/test_harmonic.cpp
@@ -3,13 +3,17 @@
 #include "doctest.h"
 #include "harmonic.hpp"
 
+#define CHECK_CLOSE(a,b,eps) CHECK((a) == doctest::Approx(b).epsilon(eps))
+
 TEST_CASE("test_fixed_values")
 {
    using polylogarithm::harmonic;
 
-   CHECK(harmonic(1) == 1.0);
-   CHECK(harmonic(2) == 1.5);
-   CHECK(harmonic(19) == 3.5477396571436819);
-   CHECK(harmonic(20) == 3.5977396571436819);
-   CHECK(harmonic(100) == 5.1873775176396203);
+   const double eps = 1e-15;
+
+   CHECK_CLOSE(harmonic(1), 1.0, eps);
+   CHECK_CLOSE(harmonic(2), 1.5, eps);
+   CHECK_CLOSE(harmonic(19), 3.5477396571436819, eps);
+   CHECK_CLOSE(harmonic(20), 3.5977396571436819, eps);
+   CHECK_CLOSE(harmonic(100), 5.1873775176396203, eps);
 }
