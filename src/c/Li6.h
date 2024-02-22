9f7265eb25e70d08a39b9927d0192b95e3ef4854
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-02-22 14:17:36 +0100

    adding C headers for polylogarithms

---
+++ src/c/Li6.h
@@ -0,0 +1,21 @@
+/* ====================================================================
+ * This file is part of Polylogarithm.
+ *
+ * Polylogarithm is licenced under the MIT License.
+ * ==================================================================== */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** complex polylogarithm with n=4 with double precision */
+double _Complex cli6(double _Complex);
+
+/** complex polylogarithm with n=4 with long double precision */
+long double _Complex cli6l(long double _Complex);
+
+#ifdef __cplusplus
+}
+#endif
