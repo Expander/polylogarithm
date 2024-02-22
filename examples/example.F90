276f58f0bd99312da2f4fc6be33121f678fc6c3f
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-02-22 14:51:38 +0100

    adding FORTRAN headers

--- examples/example.F90
+++ examples/example.F90
@@ -5,6 +5,11 @@
 program example
   implicit none
 
+#include "polylogarithm/Cl2.fh"
+#include "polylogarithm/Cl3.fh"
+#include "polylogarithm/Cl4.fh"
+#include "polylogarithm/Cl5.fh"
+#include "polylogarithm/Cl6.fh"
 #include "polylogarithm/Li2.fh"
 #include "polylogarithm/Li3.fh"
 #include "polylogarithm/Li4.fh"
@@ -17,6 +22,11 @@ program example
   x = 1.1D0
   z = dcmplx(1.1D0, 1.1D0)
 
+  print *, 'dcl2(', x, ') = ', dcl2(x)
+  print *, 'dcl3(', x, ') = ', dcl3(x)
+  print *, 'dcl4(', x, ') = ', dcl4(x)
+  print *, 'dcl5(', x, ') = ', dcl5(x)
+  print *, 'dcl6(', x, ') = ', dcl6(x)
   print *, 'dli2(', x, ') = ', dli2(x)
   print *, 'dli3(', x, ') = ', dli3(x)
   print *, 'dli4(', x, ') = ', dli4(x)
