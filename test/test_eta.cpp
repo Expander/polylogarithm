1a417aeaaaf06931b88ae66ad2da0ec8e33c9af1
Alexander Voigt <Expander@users.noreply.github.com>
2024-03-05 17:26:19 +0100

    Increase coverage (#41)

--- test/test_eta.cpp
+++ test/test_eta.cpp
@@ -2,11 +2,25 @@
 
 #include "doctest.h"
 #include "eta.hpp"
+#include "test.hpp"
+#include <cmath>
 
 TEST_CASE("test_fixed_values")
 {
    using polylogarithm::neg_eta;
 
+   if (has_inf()) {
+      CHECK(std::isinf(neg_eta(-221)));
+      CHECK(std::isinf(neg_eta(-219)));
+   }
+   CHECK(neg_eta(-222) == 0.0);
+   CHECK(neg_eta(-220) == 0.0);
+   CHECK(neg_eta(-218) == 0.0);
+   CHECK(neg_eta(-110) == 0.0);
+   CHECK(neg_eta(-3) == 1.0/8);
+   CHECK(neg_eta(-2) == 0.0);
+   CHECK(neg_eta(-1) == -0.25);
+   CHECK(neg_eta(0)  == -0.5);
    CHECK(neg_eta(1)  == -0.69314718055994531);
    CHECK(neg_eta(2)  == -0.82246703342411322);
    CHECK(neg_eta(52) == -0.9999999999999998);
