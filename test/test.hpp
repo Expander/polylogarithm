c042e617f36e2520de7b11e9e2b363bbfcb4c698
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-03-06 11:36:33 +0100

    test only for existance of inf and nan

--- test/test.hpp
+++ test/test.hpp
@@ -50,9 +50,7 @@ inline bool has_signed_zero() noexcept
 
 inline bool is_ieee754_compliant() noexcept
 {
-   constexpr bool no_fast_math = (1.0 + 0.1) - 1.0 != 0.1;
-
-   return has_inf() && has_signed_zero() && no_fast_math;
+   return has_inf() && has_signed_zero();
 }
 
 
